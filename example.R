bceecd56dc61f174fc2676941858d606af6a9da4
Frank Aragona <92396451+DOH-FAA3303@users.noreply.github.com>
2025-06-03 17:07:38 -0700

    feat: change filter to 1985, add df2
    this will make a filter change to limit which records we want in the df

--- example.R
+++ example.R
@@ -2,4 +2,8 @@ library(dplyr)
 
 # here's an example R script
 df <- dplyr::storms |>
-  filter(year > '1975')
+  filter(year > '1985')
+
+# here's an addition to the script
+df2 <- df |>
+  select(col)
